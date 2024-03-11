e2037c1efc001a1bc4b12e9f3a364a74e49e82e5
nick momotenko <nickmomotenkoforweb@gmail.com>
2024-03-11 15:25:44 +0300

    task1 fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,13 @@
-import {
-  Panel,
-  PanelHeader,
-  Group,
-  CellButton,
-  View,
-  Input,
-  FormItem,
-  Button,
-  Spinner,
-  Text,
-} from "@vkontakte/vkui";
-import { useRef, useState } from "react";
-import { useData } from "./api/hooks/useData";
+import { View } from "@vkontakte/vkui";
+import { useState } from "react";
+import { Task1 } from "./containers/Task1";
 
 export const App = () => {
   const [activePanel, setActivePanel] = useState<string>("panel1");
 
-  const textInputRef = useRef<HTMLInputElement>(null);
-
-  const { error, load, getData } = useData();
-
-  const submitForm = () => {
-    getData().then((fact) => {
-      if (fact) {
-        setCursorPlaceByText(fact);
-      }
-    });
-  };
-
-  const setCursorPlaceByText = (text: string) => {
-    if (textInputRef && textInputRef.current) {
-      let firstWordLength = text?.split(" ")[0].length;
-
-      textInputRef.current.value = text;
-
-      setTimeout(() => {
-        textInputRef.current?.setSelectionRange(
-          firstWordLength,
-          firstWordLength
-        );
-        textInputRef.current?.focus();
-      }, 0);
-    }
-  };
-
   return (
     <View activePanel={activePanel}>
-      <Panel id="panel1">
-        <PanelHeader style={{ marginBottom: 0 }}>Panel 1</PanelHeader>
-        <Group>
-          <FormItem htmlFor="example" top="Введите что нибудь">
-            <Input
-              id="example"
-              type="text"
-              placeholder="..."
-              getRef={textInputRef}
-              disabled={load}
-              onChange={() => {}}
-            />
-          </FormItem>
-          {error && <Text>{error}</Text>}
-          <Button onClick={submitForm} disabled={load} style={{ margin: 16 }}>
-            {load ? (
-              <Spinner size="small" style={{ color: "#fff" }} />
-            ) : (
-              "Click"
-            )}
-          </Button>
-          <CellButton onClick={() => setActivePanel("panel2")}>
-            Go to panel 2
-          </CellButton>
-        </Group>
-      </Panel>
+      <Task1 id="panel1" setActivePanel={setActivePanel} />
     </View>
   );
 };
--- src/containers/Task1/index.tsx
+++ src/containers/Task1/index.tsx
@@ -1,3 +1,75 @@
-export const Task1 = () => {
-  return <div>index</div>;
+import { useRef } from "react";
+
+import {
+  Panel,
+  PanelHeader,
+  Group,
+  FormItem,
+  Input,
+  Button,
+  Spinner,
+  CellButton,
+  Text,
+} from "@vkontakte/vkui";
+
+import { useData } from "../../api/hooks/useData";
+
+type Task1Props = {
+  setActivePanel: (value: string) => void;
+  id: string;
+};
+
+export const Task1: React.FC<Task1Props> = ({ setActivePanel, id }) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const { error, load, getData } = useData();
+
+  const submitForm = () => {
+    getData().then((fact) => {
+      if (fact) {
+        setCursorPlaceByText(fact);
+      }
+    });
+  };
+
+  const setCursorPlaceByText = (text: string) => {
+    if (textInputRef && textInputRef.current) {
+      let firstWordLength = text?.split(" ")[0].length;
+
+      textInputRef.current.value = text;
+
+      setTimeout(() => {
+        textInputRef.current?.setSelectionRange(
+          firstWordLength,
+          firstWordLength
+        );
+        textInputRef.current?.focus();
+      }, 0);
+    }
+  };
+
+  return (
+    <Panel id={id}>
+      <PanelHeader style={{ marginBottom: 0 }}>Panel 1</PanelHeader>
+      <Group>
+        <FormItem htmlFor="example" top="Введите что нибудь">
+          <Input
+            id="example"
+            type="text"
+            placeholder="..."
+            getRef={textInputRef}
+            disabled={load}
+            onChange={() => {}}
+          />
+        </FormItem>
+        {error && <Text>{error}</Text>}
+        <Button onClick={submitForm} disabled={load} style={{ margin: 16 }}>
+          {load ? <Spinner size="small" style={{ color: "#fff" }} /> : "Click"}
+        </Button>
+        <CellButton onClick={() => setActivePanel("panel2")}>
+          Go to panel 2
+        </CellButton>
+      </Group>
+    </Panel>
+  );
 };
